d485f61b56c761c864c87b8b7b029b01219200c9
vivekkc91 <150071373+vivekkc91@users.noreply.github.com>
2023-11-07 00:16:55 +0530

    First commit
    Working simulation of LM25 project :  https://www.tinkercad.com/things/2UYHdgHxus8-copy-of-lm35-temperature-sensor/editel?sharecode=8FyM5Oe3F7hC23cfvSB_U1q_G54r7e-G7x3CLoNETRw

---
+++ blink_led_lm35_temperature_sensor1.ino
@@ -0,0 +1,48 @@
+#define LED_pin 13 // Onboard Led pin
+#define TEMPERATURE_threshold 30 // Temperature threshold in degrees Celsius
+
+int sensorPin = A0; // LM35 temperature sensor pin
+
+void setup() {
+  pinMode(LED_pin, OUTPUT); // set led pin as output
+  Serial.begin(9600); // initialize serial communication
+}
+
+void loop() {
+  int temp = getTemperature(); // Read temperature from the sensor
+  Serial.print("Temperature: ");
+  Serial.println(temp);
+  
+  if (temp < TEMPERATURE_threshold) {
+    blinkLED(250); // Blink LED every 250 milliseconds
+  } else {
+    blinkLED(500); // Blink LED every 500 milliseconds
+  }
+}
+
+int getTemperature() {
+  int sensorValue = analogRead(sensorPin); // Read analog value from LM35 sensor
+  
+  // Convert analog value to temperature in degrees Celsius
+  float voltage = (sensorValue / 1023.0) * 5.0;
+  int temp = (voltage - 0.5) * 100;
+  
+  return temp;
+}
+
+void blinkLED(int interval) {
+  static unsigned long previousMillis = 0;
+  static bool ledState = false;
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    
+    digitalWrite(LED_pin, ledState);
+    delay(interval); // Delay for the specified interval
+  }
+}
+
+
