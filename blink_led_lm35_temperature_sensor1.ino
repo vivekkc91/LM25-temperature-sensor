d4956d83f128683164d878b1e7304cb1169b549b
vivekkc91 <150071373+vivekkc91@users.noreply.github.com>
2023-11-07 04:38:33 +0530

    Update blink_led_lm35_temperature_sensor1.ino

--- blink_led_lm35_temperature_sensor1.ino
+++ blink_led_lm35_temperature_sensor1.ino
@@ -1,29 +1,22 @@
-#include <TimerOne.h> // Include the Timer1 library
-
-#define LED_pin 13 // Onboard LED pin
+#define LED_pin 13 // Onboard Led pin
 #define TEMPERATURE_threshold 30 // Temperature threshold in degrees Celsius
 
 int sensorPin = A0; // LM35 temperature sensor pin
-int interval = 250; // Initial interval for blinking
-bool ledState = false;
 
 void setup() {
-  pinMode(LED_pin, OUTPUT); // Set LED pin as output
-  Serial.begin(9600); // Initialize serial communication
-
-  Timer1.initialize(interval * 1000); // Initialize Timer1 with interval in microseconds
-  Timer1.attachInterrupt(blinkLED); // Attach the blinkLED function to the timer interrupt
+  pinMode(LED_pin, OUTPUT); // set led pin as output
+  Serial.begin(9600); // initialize serial communication
 }
 
 void loop() {
   int temp = getTemperature(); // Read temperature from the sensor
   Serial.print("Temperature: ");
   Serial.println(temp);
-
+  
   if (temp < TEMPERATURE_threshold) {
-    interval = 250; // Blink LED every 250 milliseconds
+    blinkLED(250); // Blink LED every 250 milliseconds
   } else {
-    interval = 500; // Blink LED every 500 milliseconds
+    blinkLED(500); // Blink LED every 500 milliseconds
   }
 }
 
@@ -37,7 +30,18 @@ int getTemperature() {
   return temp;
 }
 
-void blinkLED() {
-  ledState = !ledState;
-  digitalWrite(LED_pin, ledState);
+void blinkLED(int interval) {
+  static unsigned long previousMillis = 0;
+  static bool ledState = false;
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    
+    digitalWrite(LED_pin, ledState);
+    delay(interval); 
+  }
 }
+
